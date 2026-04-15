d612097b30b4a83f4b1ab9adb8f9f3ff846ba384
Rodrigo F <rodrigo.fc773@gmail.com>
2026-04-15 19:30:28 -0300

    first commit

---
+++ src/main/java/com/fiap/cheffyorderservice/CheffyOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.cheffyorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheffyOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheffyOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fiap/cheffyorderservice/CheffyOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.cheffyorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheffyOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
